9f05abe6d01fa3b5702884fd2946f86075ab89f6
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-05-28 01:55:31 -0300

    creo los respositorios para taxi y omnibus

---
+++ App_Transporte/App_Transporte.Datos/RepositorioOmnibus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App_Transporte.Entidades;
+
+namespace App_Transporte.Datos
+{
+    public class RepositorioOmnibus
+    {
+        private List<Omnibus> omnibuses;
+
+        public RepositorioOmnibus()
+        {
+            omnibuses = new List<Omnibus>();
+        }
+
+        public void agregarOmnibus(Omnibus bus)
+        {
+            omnibuses.Add(bus);
+        }
+
+        public void mostrarOmnibus()
+        {
+            int contador = 0;
+            while (contador < 5)
+            {
+                if (contador < omnibuses.Count)
+                {
+                    omnibuses[contador].MostrarDatos();
+                    contador++;
+                }
+                else
+                {
+                    contador++;
+                    Console.WriteLine($"Omnibus {contador}: 0 pasajeros");
+                }
+            }
+        }
+    }
+}
---
+++ App_Transporte/App_Transporte.Datos/RepositorioTaxi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App_Transporte.Entidades;
+
+namespace App_Transporte.Datos
+{
+    public class RepositorioTaxi
+    {
+        private List<Taxi> Taxis;
+
+        public RepositorioTaxi()
+        {
+            Taxis = new List<Taxi>();
+        }
+        public void agregarTaxi(Taxi taxi)
+        {
+            Taxis.Add(taxi);
+        }
+
+        public void mostrarTaxis()
+        {
+            int contador = 0;
+            while (contador < 5)
+            {
+                if (contador < Taxis.Count)
+                {
+                    Taxis[contador].MostrarDatos();
+                    contador++;
+                }
+                else
+                {
+                    contador++;
+                    Console.WriteLine($"Taxi {contador}: 0 pasajeros");
+
+                }
+            }
+        }
+
+    }
+}
