e54f58771b9868ee97b0817922b3cc78c511b90d
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-05-31 00:14:42 -0300

    inicializo proyecto de consola

---
+++ Practica_TP2/PracticaTP2.Presentacion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaTP2.Presentacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
