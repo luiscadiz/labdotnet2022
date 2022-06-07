608602a48183c6507fef79ab221e1b4be92b1733
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-06-07 03:10:09 -0300

    muestra todos los customers obtenidos.

--- App.TP4/App.TP4.UI/Program.cs
+++ App.TP4/App.TP4.UI/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using App.TP4.Entities;
+using App.TP4.Logic;
 
 namespace App.TP4.UI
 {
@@ -10,6 +12,14 @@ namespace App.TP4.UI
     {
         static void Main(string[] args)
         {
+            CustomersLogic customersLogic = new CustomersLogic();
+
+            foreach(Customers customer in customersLogic.GetAll())
+            {
+                Console.WriteLine($"{customer.ContactName} - {customer.Address}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
