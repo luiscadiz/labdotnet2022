976f530aa1869282c2c85d9673d0170ebaac0c65
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-06-07 02:52:23 -0300

    actualiza en la capa de datos ef. a la version 6.4.4

---
+++ App.TP4/App.TP4.Logic/CustomersLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.TP4.Data;
+using App.TP4.Entities;
+
+namespace App.TP4.Logic
+{
+    public class CustomersLogic : BaseLogic
+    {
+        public List<Customers> GetAll()
+        {
+            return _context.Customers.ToList();
+        }
+    }
+}
