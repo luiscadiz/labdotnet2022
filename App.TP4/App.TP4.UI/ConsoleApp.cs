e697c38e38c629b6dbc952a68e5da2f22aa949e2
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-06-07 05:48:48 -0300

    refactor de la UI

---
+++ App.TP4/App.TP4.UI/ConsoleApp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.TP4.Entities;
+using App.TP4.Logic;
+
+namespace App.TP4.UI
+{
+    public class ConsoleApp
+    {
+        public static void Start()
+        {
+            short optionCurrent;
+            var headerHome = Headers.HeaderHome;
+            var options = Options.OptionHome;
+            Menu mainMenu = new Menu(headerHome, options);
+            do
+            {
+                mainMenu.RunMenu();
+                optionCurrent = mainMenu.SelectedIndex;
+                SelectOption(optionCurrent);
+            } while (optionCurrent >= 0 && optionCurrent <= options.Length - 2);
+        }
+
+        private static void SelectOption(int optionCurrent)
+        {
+            switch (optionCurrent)
+            {
+                case 0:
+                    OptionOne();
+                    break;
+                case 1:
+
+                    break;
+            }
+        }
+        private static void OptionOne()
+        {
+            Menu menuEntities = new Menu(Headers.HeaderOptionOne, Options.OptionsOne);
+            menuEntities.RunMenu();
+            switch (menuEntities.SelectedIndex)
+            {
+                case 0:
+                    CustomersLogic customersLogic = new CustomersLogic();
+                    customersLogic.ShowAll();
+                    Console.ReadLine();
+                    break;
+                case 1:
+                    
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- App.TP4/App.TP4.UI/Program.cs
+++ App.TP4/App.TP4.UI/Program.cs
@@ -3,23 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using App.TP4.Entities;
-using App.TP4.Logic;
 
 namespace App.TP4.UI
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            CustomersLogic customersLogic = new CustomersLogic();
-
-            foreach(Customers customer in customersLogic.GetAll())
-            {
-                Console.WriteLine($"{customer.ContactName} - {customer.Address}");
-            }
-
-            Console.ReadLine();
+            ConsoleApp.Start();
         }
     }
 }
