e06e5ce9a4c9d398d99e9e2fb150b494b63e6238
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-06-07 02:36:24 -0300

    (feat, BaseLogic.cs): crea una clase base para la logica de las entidades.

---
+++ App.TP4/App.TP4.Logic/BaseLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.TP4.Data;
+
+namespace App.TP4.Logic
+{
+    public abstract class BaseLogic
+    {
+        protected readonly NorthwindContext _context;
+
+        public BaseLogic()
+        {
+            _context = new NorthwindContext();
+        }
+    }
+}
