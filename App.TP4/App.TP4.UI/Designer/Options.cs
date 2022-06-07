3c8f6d9c54b27b70953b8f407648fed588421f23
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-06-07 05:44:40 -0300

    crea clases para el diseño de la interfaz.

---
+++ App.TP4/App.TP4.UI/Designer/Options.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.TP4.UI
+{
+    public class Options
+    {
+        private static string[] _optionsHome =
+                                       {"Consultar datos",
+                                        "Insertar",
+                                        "Actualizar",
+                                        "Eliminar",
+                                        "Salir del programa"};
+
+        private static string[] _optionsOne = { "Clientes", "Empleados", "Volver" };
+
+        public static string[] OptionHome { get => _optionsHome;}
+        public static string[] OptionsOne { get => _optionsOne;}
+    }
+}
