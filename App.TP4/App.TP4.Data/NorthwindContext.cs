71743537971b4d7533f8aa54fc8c51eddfb35352
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-06-07 02:02:02 -0300

    realiza el mapeo usando code first Las tablas que se mapearon son la tabla de Categories, Customers, Employees y Suppliers.

---
+++ App.TP4/App.TP4.Data/NorthwindContext.cs
@@ -0,0 +1,32 @@
+namespace App.TP4.Data
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+            : base("name=NorthwindConnection")
+        {
+        }
+
+        public virtual DbSet<Categories> Categories { get; set; }
+        public virtual DbSet<Customers> Customers { get; set; }
+        public virtual DbSet<Employees> Employees { get; set; }
+        public virtual DbSet<Suppliers> Suppliers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customers>()
+                .Property(e => e.CustomerID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Employees>()
+                .HasMany(e => e.Employees1)
+                .WithOptional(e => e.Employees2)
+                .HasForeignKey(e => e.ReportsTo);
+        }
+    }
+}
