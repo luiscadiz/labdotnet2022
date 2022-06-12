5094bf88dccab78e3e7216e3dc9aabc48a08037e
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-06-12 14:07:56 -0300

    agrega referencia a ef en las capas

---
+++ Practico.TP5/Practico.TP5.Logic/CustomerLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Practico.TP5.Datos;
+using Practico.TP5.Entities;
+
+namespace Practico.TP5.Logic
+{
+    public class CustomerLogic
+    {
+        private readonly NorthwindContext _context;
+
+        public CustomerLogic()
+        {
+            _context = new NorthwindContext();
+        }
+        public List<Customers> GetAll()
+        {
+            IQueryable<Customers> queryGetAllCustomer = from customer in _context.Customers
+                                                        select customer;
+            return queryGetAllCustomer.ToList();
+        }
+
+    }
+}
