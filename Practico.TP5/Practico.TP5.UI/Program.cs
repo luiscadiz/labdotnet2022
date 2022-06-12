d6edccf1f1012b0bea45c0ba7aa5a7a65c264020
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-06-12 14:50:58 -0300

    referencia la cad. de conexion a la capa ui

--- Practico.TP5/Practico.TP5.UI/Program.cs
+++ Practico.TP5/Practico.TP5.UI/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Practico.TP5.Entities;
+using Practico.TP5.Logic;
 
 namespace Practico.TP5.UI
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            var customerLogic = new CustomerLogic();
+
+            var allCustormers = customerLogic.GetAll();
+
+            foreach(var customer in allCustormers)
+            {
+                Console.WriteLine($"{customer.CompanyName} - {customer.ContactName}");
+            }
+
+            Console.ReadLine();
+            
+
         }
     }
 }
