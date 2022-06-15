2ddc2fd3d12af1f3310d4e77f0e946225640b84e
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-06-15 04:35:14 -0300

    arregla tamño del formulario

--- Formulario_TP6/Js/Main.js
+++ Formulario_TP6/Js/Main.js
@@ -4,7 +4,7 @@ const inputs = document.querySelectorAll('#form input');
 const btn_clean = document.getElementById('form_btn_clear');
 
 const expressions = {
-	names: /^[a-zA-ZÀ-ÿ\s]{2,40}$/, // Letras y espacios, pueden llevar acentos.
+	names: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	// password: /^.{4,12}$/, // 4 a 12 digitos.
 	// correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	// telefono: /^\d{7,14}$/ // 7 a 14 numeros.
