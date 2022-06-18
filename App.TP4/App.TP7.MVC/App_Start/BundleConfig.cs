9b1fbad9f6d5b3a2e365751832012298410337fb
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-06-17 22:44:42 -0300

    arregla problemas del template

--- App.TP4/App.TP7.MVC/App_Start/BundleConfig.cs
+++ App.TP4/App.TP7.MVC/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace App.TP7.MVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/scriptsTemplate.js"
+                        /*"~/Scripts/jquery-{version}.js"*/));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -23,8 +24,11 @@ namespace App.TP7.MVC
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Boostrap.css",
+                      "~/Content/Site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Template/css").Include(
+                     "~/Content/Template/stylesTemplate.css"));
         }
     }
 }
