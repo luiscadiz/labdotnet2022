06f44d66d221cb85fa3fd716b4fed89daad5007e
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-06-21 22:37:08 -0300

    crea capa de webapi

---
+++ App.TP4/App.TP8.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace App.TP8.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ App.TP4/App.TP8.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="App.TP8.WebAPI.WebApiApplication" Language="C#" %>
