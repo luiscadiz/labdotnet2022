97431acb896bb213070549a96119bc119ac31f5c
Luis Cadiz <luiscadiz1504@hotmail.com>
2022-06-21 22:19:59 -0300

    temina punto uno

--- App.TP4/App.TP7.MVC/Controllers/HomeController.cs
+++ App.TP4/App.TP7.MVC/Controllers/HomeController.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Text;
+using Newtonsoft;
+using System.Net.Http;
+using System.Threading.Tasks;
+using App.TP7.MVC.Models;
+using Newtonsoft.Json;
+using App.TP4.Entities;
+using System.Net;
 
 namespace App.TP7.MVC.Controllers
 {
@@ -10,7 +18,12 @@ namespace App.TP7.MVC.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var url = "https://rickandmortyapi.com/api/character";
+            WebClient client = new WebClient();
+            var datos = client.DownloadString(url);
+            var lista = JsonConvert.DeserializeObject<ModelsRickAndMortyView>(datos);
+            return View(lista);
+
         }
 
         public ActionResult About()
